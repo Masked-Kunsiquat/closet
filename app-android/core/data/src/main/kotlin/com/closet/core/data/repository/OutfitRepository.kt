94b64c33968b2e14c55d4e156b4415432f532088
Masked-Kunsiquat <130736043+Masked-Kunsiquat@users.noreply.github.com>
2026-03-19 22:08:56 -0400

    fix(data): correct OutfitRepository entity import paths

--- app-android/core/data/src/main/kotlin/com/closet/core/data/repository/OutfitRepository.kt
+++ app-android/core/data/src/main/kotlin/com/closet/core/data/repository/OutfitRepository.kt
@@ -3,9 +3,6 @@ package com.closet.core.data.repository
 import androidx.room.withTransaction
 import com.closet.core.data.ClothingDatabase
 import com.closet.core.data.dao.OutfitDao
-import com.closet.core.data.entity.OutfitEntity
-import com.closet.core.data.entity.OutfitItemEntity
-import com.closet.core.data.entity.OutfitWithItems
 import com.closet.core.data.model.*
 import com.closet.core.data.util.AppError
 import com.closet.core.data.util.DataResult
